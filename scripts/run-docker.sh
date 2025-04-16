6477c68d2a2d2fcc65913dd7828513a9e2a50b29
JamyJones <93288699+JamyJones@users.noreply.github.com>
2025-04-16 07:32:16 +0300

    commited changes

--- scripts/run-docker.sh
+++ scripts/run-docker.sh
@@ -53,4 +53,8 @@ if [ "$#" -eq "0" ]; then
 	set -- bash
 fi
 
+echo $DOCKER_EXEC_PID_FILE_PATH
+docker inspect --format '{{.State.Pid}}' termux-package-builder-legacy
+
+exit 0
 $SUDO docker exec --env "DOCKER_EXEC_PID_FILE_PATH=$DOCKER_EXEC_PID_FILE_PATH" --interactive $DOCKER_TTY $CONTAINER_NAME "$@"
