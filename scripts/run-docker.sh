2815887384f4bd58d801977715b3ef894927660a
JamyJones <93288699+JamyJones@users.noreply.github.com>
2025-04-16 06:00:46 +0300

    commited changes

--- scripts/run-docker.sh
+++ scripts/run-docker.sh
@@ -11,7 +11,7 @@ if [ "$UNAME" = Darwin ]; then
 	SEC_OPT=""
 else
 	REPOROOT="$(dirname $(readlink -f $0))/../"
-	SEC_OPT=" --security-opt seccomp=$REPOROOT/scripts/profile.json"
+	SEC_OPT=" --security-opt seccomp=${REPOROOT}scripts/profile.json"
 fi
 
 # Required for Linux with SELinux and btrfs to avoid permission issues, eg: Fedora
