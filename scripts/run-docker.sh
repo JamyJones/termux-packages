d294027f92ff93b38aafa400432646064bd90543
JamyJones <93288699+JamyJones@users.noreply.github.com>
2025-04-16 05:56:15 +0300

    commited changes

--- scripts/run-docker.sh
+++ scripts/run-docker.sh
@@ -1,44 +1,74 @@
 #!/bin/sh
 set -e -u
 
-HOME=/home/builder
+TERMUX_SCRIPTDIR=$(cd "$(realpath "$(dirname "$0")")"; cd ..; pwd)
+
+CONTAINER_HOME_DIR=/home/builder
 UNAME=$(uname)
 if [ "$UNAME" = Darwin ]; then
 	# Workaround for mac readlink not supporting -f.
 	REPOROOT=$PWD
+	SEC_OPT=""
 else
 	REPOROOT="$(dirname $(readlink -f $0))/../"
+	SEC_OPT=" --security-opt seccomp=$REPOROOT/scripts/profile.json"
 fi
 
-: ${TERMUX_BUILDER_IMAGE_NAME:="termux/package-builder:legacy"}
+# Required for Linux with SELinux and btrfs to avoid permission issues, eg: Fedora
+# To reset, use "restorecon -Fr ."
+# To check, use "ls -Z ."
+if [ -n "$(command -v getenforce)" ] && [ "$(getenforce)" = Enforcing ]; then
+	VOLUME=$REPOROOT:$CONTAINER_HOME_DIR/termux-packages:z
+else
+	VOLUME=$REPOROOT:$CONTAINER_HOME_DIR/termux-packages
+fi
+
+: ${TERMUX_BUILDER_IMAGE_NAME:=termux/package-builder:legacy}
 : ${CONTAINER_NAME:=termux-package-builder-legacy}
 
 USER=builder
 
-echo "Running container '$CONTAINER_NAME' from image '$TERMUX_BUILDER_IMAGE_NAME'..."
-
-docker start $CONTAINER_NAME > /dev/null 2> /dev/null || {
-	echo "Creating new container..."
-	docker run \
-		--detach \
-		--name $CONTAINER_NAME \
-		--volume $REPOROOT:$HOME/termux-packages \
-		$TERMUX_BUILDER_IMAGE_NAME
-    if [ "$UNAME" != Darwin ]; then
-	if [ $(id -u) -ne 1000 -a $(id -u) -ne 0 ]
-	then
-		echo "Changed builder uid/gid... (this may take a while)"
-		docker exec  $CONTAINER_NAME sudo chown -R $(id -u) $HOME
-		docker exec  $CONTAINER_NAME sudo chown -R $(id -u) /data
-		docker exec  $CONTAINER_NAME sudo usermod -u $(id -u) builder
-		docker exec  $CONTAINER_NAME sudo groupmod -g $(id -g) builder
-	fi
-    fi
-}
-
-if [ "$#" -eq  "0" ]; then
-	docker exec   $CONTAINER_NAME bash
+if [ -n "${TERMUX_DOCKER_USE_SUDO-}" ]; then
+	SUDO="sudo"
 else
-	docker exec   $CONTAINER_NAME $@
+	SUDO=""
 fi
 
+echo "Running container '$CONTAINER_NAME' from image '$TERMUX_BUILDER_IMAGE_NAME'..."
+
+# Check whether attached to tty and adjust docker flags accordingly.
+if [ -t 1 ]; then
+	DOCKER_TTY=" --tty"
+else
+	DOCKER_TTY=""
+fi
+
+$SUDO docker start $CONTAINER_NAME >/dev/null 2>&1 || {
+	echo "Creating new container..."
+	$SUDO docker run \
+		--detach \
+		--init \
+		--name $CONTAINER_NAME \
+		--volume $VOLUME \
+		$SEC_OPT \
+		--tty \
+		$TERMUX_BUILDER_IMAGE_NAME
+	if [ "$UNAME" != Darwin ]; then
+		if [ $(id -u) -ne 1001 -a $(id -u) -ne 0 ]; then
+			echo "Changed builder uid/gid... (this may take a while)"
+			$SUDO docker exec $DOCKER_TTY $CONTAINER_NAME sudo chown -R $(id -u) $CONTAINER_HOME_DIR
+			$SUDO docker exec $DOCKER_TTY $CONTAINER_NAME sudo chown -R $(id -u) /data
+			$SUDO docker exec $DOCKER_TTY $CONTAINER_NAME sudo usermod -u $(id -u) builder
+			$SUDO docker exec $DOCKER_TTY $CONTAINER_NAME sudo groupmod -g $(id -g) builder
+		fi
+	fi
+}
+
+# Set traps to ensure that the process started with docker exec and all its children are killed.
+. "$TERMUX_SCRIPTDIR/scripts/utils/docker/docker.sh"; docker__setup_docker_exec_traps
+
+if [ "$#" -eq "0" ]; then
+	set -- bash
+fi
+
+$SUDO docker exec --env "DOCKER_EXEC_PID_FILE_PATH=$DOCKER_EXEC_PID_FILE_PATH" --interactive $DOCKER_TTY $CONTAINER_NAME "$@"
