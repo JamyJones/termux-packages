6379cd068ceef2e838987f697441efe16038bd7f
JamyJones <93288699+JamyJones@users.noreply.github.com>
2025-04-16 06:38:50 +0300

    commited changes

--- scripts/run-docker.sh
+++ scripts/run-docker.sh
@@ -14,29 +14,37 @@ fi
 : ${CONTAINER_NAME:=termux-package-builder-legacy}
 
 USER=builder
-
+if [ -n "${TERMUX_DOCKER_USE_SUDO-}" ]; then
+	SUDO="sudo"
+else
+	SUDO=""
+fi
 echo "Running container '$CONTAINER_NAME' from image '$TERMUX_BUILDER_IMAGE_NAME'..."
-
-docker start $CONTAINER_NAME > /dev/null 2> /dev/null || {
+$SUDO docker start $CONTAINER_NAME >/dev/null 2>&1 || {
 	echo "Creating new container..."
-	docker run \
+	$SUDO docker run \
 		--detach \
+		--init \
 		--name $CONTAINER_NAME \
-		--volume $REPOROOT:$HOME/termux-packages \
+		--volume $VOLUME \
+		--tty \
 		$TERMUX_BUILDER_IMAGE_NAME
-    if [ "$UNAME" != Darwin ]; then
-	if [ $(id -u) -ne 1000 -a $(id -u) -ne 0 ]
-	then
-		echo "Changed builder uid/gid... (this may take a while)"
-		docker exec  $CONTAINER_NAME sudo chown -R $(id -u) $HOME
-		docker exec  $CONTAINER_NAME sudo chown -R $(id -u) /data
-		docker exec  $CONTAINER_NAME sudo usermod -u $(id -u) builder
-		docker exec  $CONTAINER_NAME sudo groupmod -g $(id -g) builder
+	if [ "$UNAME" != Darwin ]; then
+		if [ $(id -u) -ne 1001 -a $(id -u) -ne 0 ]; then
+			echo "Changed builder uid/gid... (this may take a while)"
+			$SUDO docker exec $DOCKER_TTY $CONTAINER_NAME sudo chown -R $(id -u) $CONTAINER_HOME_DIR
+			$SUDO docker exec $DOCKER_TTY $CONTAINER_NAME sudo chown -R $(id -u) /data
+			$SUDO docker exec $DOCKER_TTY $CONTAINER_NAME sudo usermod -u $(id -u) builder
+			$SUDO docker exec $DOCKER_TTY $CONTAINER_NAME sudo groupmod -g $(id -g) builder
+		fi
 	fi
-    fi
 }
+
+# Set traps to ensure that the process started with docker exec and all its children are killed.
+#. "$TERMUX_SCRIPTDIR/scripts/utils/docker/docker.sh"; docker__setup_docker_exec_traps
+
 if [ "$#" -eq "0" ]; then
 	set -- bash
 fi
 
-$SUDO docker exec --env "DOCKER_EXEC_PID_FILE_PATH=$DOCKER_EXEC_PID_FILE_PATH" --interactive  $CONTAINER_NAME "$@"
+$SUDO docker exec --env "DOCKER_EXEC_PID_FILE_PATH=$DOCKER_EXEC_PID_FILE_PATH" --interactive $DOCKER_TTY $CONTAINER_NAME "$@"
