575dc226c3fd5caab8c748a6a495374ba4faa666
JamyJones <93288699+JamyJones@users.noreply.github.com>
2025-04-18 21:15:27 +0300

    commited changes

--- packages/pdfmium_viewer/build.sh
+++ packages/pdfmium_viewer/build.sh
@@ -1,3 +1,4 @@
+apt-get update -y && apt-get upgrade -y
 TERMUX_PKG_HOMEPAGE=https://github.com/John4650-hub/my-Termux-packs
 TERMUX_PKG_DESCRIPTION="A pdfViewer using pdfmium"
 TERMUX_PKG_LICENSE="Apache-2.0"
