357c283d0059fc5c5f1396716a8fca53f59b8a49
JamyJones <93288699+JamyJones@users.noreply.github.com>
2025-04-19 06:21:05 +0300

    commited changes

--- scripts/setup-android-sdk.sh
+++ scripts/setup-android-sdk.sh
@@ -7,6 +7,7 @@ set -e -u
 # Install desired parts of the Android SDK:
 . $(cd "$(dirname "$0")"; pwd)/properties.sh
 TERMUX_SDK_REVISION=9123335
+TERMUX_NDK_VERSION="27c"
 ANDROID_SDK_FILE=commandlinetools-linux-${TERMUX_SDK_REVISION}_latest.zip
 ANDROID_SDK_SHA256=0bebf59339eaa534f4217f8aa0972d14dc49e7207be225511073c661ae01da0a
 if [ "$TERMUX_NDK_VERSION" = "27c" ]; then
