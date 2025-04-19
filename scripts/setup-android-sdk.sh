fceb8fbf1e36351a2ebb23f85423f7915a9bbed2
JamyJones <93288699+JamyJones@users.noreply.github.com>
2025-04-19 08:07:17 +0300

    commited changes

--- scripts/setup-android-sdk.sh
+++ scripts/setup-android-sdk.sh
@@ -47,10 +47,10 @@ if [ ! -d "$NDK" ]; then
 		-o ndk-r${TERMUX_NDK_VERSION}.zip
 		echo "$ANDROID_NDK_SHA256 ndk-r${TERMUX_NDK_VERSION}.zip" | sha256sum -c -
 	rm -Rf android-ndk-r$TERMUX_NDK_VERSION
-	unzip -q ndk-r${TERMUX_NDK_VERSION}.zip ./
+	unzip -q ndk-r${TERMUX_NDK_VERSION}.zip -d ./
 
 	# Remove unused parts
-	rm -Rf android-ndk-r$TERMUX_NDK_VERSION/sources/cxx-stl/system
+	rm -Rf ./sources/cxx-stl/system
 fi
 SDK_MANAGER="$ANDROID_HOME/cmdline-tools/bin/sdkmanager"
 
